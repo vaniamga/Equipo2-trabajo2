bd8d01e4a1c68bd4f468d2cf8e8c64ac477b1a00
a01659224B <a01659224@tec.mx>
2023-07-11 10:19:47 -0600

    Conexion con la API Correcta

--- api.js
+++ api.js
@@ -1,18 +1,46 @@
 function consumir() {
+  var urls=[];
+  var names=[];
+  var ids=[]
+  var growth_times=[]; 
+  var max_harvests=[]; 
+  var natural_gift_powers=[]; 
+  var sizes=[];
+  var smoothnesses=[];  
+  var soil_drynesses=[];
     var endPoint = document.getElementById("endPoint").value;
     fetch(endPoint)
       .then(function(respuesta) {
-        return respuesta.json();
-      })
+        return respuesta.json();})
       .then(function(datos) {
-        var precios = [];
-        var categorias = [];
-        for (var i = 0; i < datos.length; i++) {
-          if (datos[i].category !== undefined && datos[i].price !== undefined) {
-            precios.push(datos[i].price);
-          }
+        console.log(datos);
+        for (var i = 0; i < datos['results'].length; i++){
+          urls.push(datos['results'][i]['url']);
         }
-        console.log(precios);
-        alert(precios);
-      });
-  }
+        for (var i = 0; i < urls.length; i++){
+          fetch(urls[i])
+            .then(function(respuesta) {
+              return respuesta.json();
+            })
+            .then(function(datos2) {
+              console.log(datos2['id']);
+              ids.push(datos2['id']);
+              console.log(datos2['name']);
+              names.push(datos2['name']);
+              console.log(datos2['growth_time']);
+              growth_times.push(datos2['growth_time']);
+              console.log(datos2['max_harvest']);
+              max_harvests.push(datos2['max_harvest']);
+              console.log(datos2['natural_gift_power']);
+              natural_gift_powers.push(datos2['natural_gift_power']);
+              console.log(datos2['size']);
+              sizes.push(datos2['size']);
+              console.log(datos2['smoothness']);
+              smoothnesses.push(datos2['smoothness']);
+              console.log(datos2['soil_dryness']);
+              soil_drynesses.push(datos2['soil_dryness']);
+            })
+        }  
+      })
+}
+  //https://pokeapi.co/api/v2/berry/
